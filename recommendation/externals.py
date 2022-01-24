722ac55d75adf6c13af28dddc20c5e404f8e3324
shubhamrevadekar05 <shubham0revadekar@gmail.com>
2022-01-24 09:12:42 +0530

    Changes in RecommendView

--- recommendation/externals.py
+++ recommendation/externals.py
@@ -1,14 +1,14 @@
 from recommender.settings import BASE_DIR
 import numpy as np
 import joblib
-""" import json
+import json
 from web3 import Web3
 
 url = 'https://rpc-mumbai.maticvigil.com'
 web3 = Web3(Web3.HTTPProvider(url))
 abi = json.loads('[{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"uint256","name":"_rank","type":"uint256"}],"name":"addRank","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256[]","name":"_ids","type":"uint256[]"},{"internalType":"uint256[]","name":"_ranks","type":"uint256[]"}],"name":"addRanks","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"uint256","name":"_rank","type":"uint256"}],"name":"dislikeDoctor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"ids","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"uint256","name":"_rank","type":"uint256"}],"name":"likeDoctor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"ranks","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]')
 address = '0x2604441A291eF0EEba319a4Fd959Fa8e96899e8E'
-contract = web3.eth.contract(address=address, abi=abi) """
+contract = web3.eth.contract(address=address, abi=abi)
 
 dataset = joblib.load(BASE_DIR / 'dataset.data')
 
@@ -26,14 +26,15 @@ def rankFilter(dataset, model, experience, fee, city_name, size):
             l.append(0.0)
     pred = np.array(l)
     rank = model.predict(pred.reshape(1,-1))
-
-    """ for i in imrange(1,rank[0]+1):
+    dataframe = pd.DataFrame()
+    for i in imrange(1,rank[0]+1):
         id = contract.functions.ids(i).call()
         entry = dataset.iloc[id,:]
-        #entry = dataset.loc[dataset['Sr No.']==id] """
+        dataframe.append(entry)
+        #entry = dataset.loc[dataset['Sr No.']==id]
 
-    dataset = dataset.loc[dataset['Rank']<=rank[0]]
-    recs = dataset.loc[dataset['city']==city_name]
+    #dataset = dataset.loc[dataset['Rank']<=rank[0]]
+    recs = dataframe.loc[dataset['city']==city_name]
     if recs['Rank'].count() < size:
         recs = recs.append(dataset.loc[dataset['city']!=city_name])
     return recs.iloc[:size,:]
