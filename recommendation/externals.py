8d8c656ff4562b4e9337e7875c81b5376a54eed1
shubhamrevadekar05 <shubham0revadekar@gmail.com>
2022-01-27 15:20:16 +0530

    rankFilter changed

--- recommendation/externals.py
+++ recommendation/externals.py
@@ -1,14 +1,14 @@
 from recommender.settings import BASE_DIR
 import numpy as np
 import joblib
-import json
+""" import json
 from web3 import Web3
 
 url = 'https://rpc-mumbai.maticvigil.com'
 web3 = Web3(Web3.HTTPProvider(url))
 abi = json.loads('[{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"uint256","name":"_rank","type":"uint256"}],"name":"addRank","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256[]","name":"_ids","type":"uint256[]"},{"internalType":"uint256[]","name":"_ranks","type":"uint256[]"}],"name":"addRanks","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"uint256","name":"_rank","type":"uint256"}],"name":"dislikeDoctor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"ids","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"uint256","name":"_rank","type":"uint256"}],"name":"likeDoctor","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"ranks","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]')
 address = '0x2604441A291eF0EEba319a4Fd959Fa8e96899e8E'
-contract = web3.eth.contract(address=address, abi=abi)
+contract = web3.eth.contract(address=address, abi=abi) """
 
 dataset = joblib.load(BASE_DIR / 'dataset.data')
 
