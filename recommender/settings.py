529b902550466f953f114f826896ad3efebc61a1
Vidhan Shah <shahvidhan123@gmail.com>
2022-06-10 10:28:05 +0530

    Cors

--- recommender/settings.py
+++ recommender/settings.py
@@ -58,8 +58,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
-    '*',
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'recommender.urls'
